68defc6766eaf3dc6451b596857d246adc21f5d7
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-05-14 10:35:11 -0700

    Fail 2nd test- getting details.

--- Business.Tests/ModelTests/OrderTests.cs
+++ Business.Tests/ModelTests/OrderTests.cs
@@ -18,5 +18,24 @@ namespace Business.Test
       Order newOrder = new Order("test", "test", "test", "test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void GetDetails_ReturnsDetails_String()
+    {
+      string name = "Zonic Cafe";
+      string description = "30 Croissants";
+      string date = "5/14/2021";
+      string price = "0.5 per croissant";
+      Order newOrder = new Order(name, description, date, price);
+      string result1 = newOrder.Name;
+      string result2 = newOrder.Description;
+      string result3 = newOrder.Date;
+      string result4 = newOrder.Price;
+      Assert.AreEqual(name, result2);
+      Assert.AreEqual(description, result2);
+      Assert.AreEqual(date, result3);
+      Assert.AreEqual(price, result4);
+      
+    }
+
   }
 }
