8873d27a4db38d42a52b7e118a0c5bc031d2e54e
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-05-14 16:35:07 -0700

    Having some errors. WIP

--- Business/Controllers/VendorsController.cs
+++ Business/Controllers/VendorsController.cs
@@ -14,11 +14,27 @@ namespace Business.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
-    [HttpPost("/vendors")]
+    [HttpPost("/vendors/new")]
+    public ActionResult New() 
+    {
+      return View();
+    }
+       [HttpPost("/vendors")]
     public ActionResult Create(string vendorName, string vendorAddress)
     {
       Vendor newVendor = new Vendor(vendorName, vendorAddress);
       return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
     }
   }
 }
